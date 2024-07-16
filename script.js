db729eb5f31c0fb54df18639618bd55e757ebb79
Yordanos Yeshitla <yordiman614@gmail.com>
2024-07-16 19:48:37 +0300

    adding two files

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".bx-menu");
+const container = document.getElementsByClassName("nav-links-container")
+
+button.addEventListener('click', ()=>{
+    container.
+})
