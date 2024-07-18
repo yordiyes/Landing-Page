a16a60419a1091723c5287c38c6b49b85f9a5236
Yordanos Yeshitla <yordiman614@gmail.com>
2024-07-18 10:11:43 +0300

    adding the third and footer part of the site

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const button = document.querySelector(".bx-menu");
-const container = document.getElementsByClassName("nav-links-container")
+document.addEventListener("DOMContentLoaded",(event0=>{
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.getElementById("nav-links");
+    const motoContainer = document.getElementById('moto-container')
 
-button.addEventListener('click', ()=>{
-    container.
+    menuIcon.addEventListener('click', ()=>{
+        navLinks.classList.toggle("active")
+        if(navLinks.classList.contains("active") )
+            motoContainer.classList.remove('top-padding');
+        else{
+            motoContainer.classList.add('top-padding');
+        }
+    })
+}))
+const navbar = document.querySelector('.nav-bar');
+const linkContainer = document.getElementById('nav-links');
+window.addEventListener('scroll', ()=>{
+    navbar.classList.toggle('navbar-on-scroll', window.scrollY > 0);
+    linkContainer.classList.toggle('nav-links-sm', window.scrollY > 0);
 })
+
+
